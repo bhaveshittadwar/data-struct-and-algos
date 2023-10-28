3b9695f403b4274fbb9edd1c10c518188708e3af
bhaveshittadwar <bitsadwar@gmail.com>
2023-10-28 01:42:34 -0400

    feat(graph): adjacency list

--- graphs/Graphs.java
+++ graphs/Graphs.java
@@ -1,7 +1,8 @@
 package graphs;
+import java.util.ArrayList;
 // Todos:
-// 1. Adjacency List - ONGOING
-// 2. Adjacency Matrix
+// 1. Adjacency List
+// 2. Adjacency Matrix - TODO
 // 3. BFS for undirected graph
 // 4. DFS for undirected graph
 // 5. DFS for directed graph
@@ -14,14 +15,34 @@ package graphs;
 // Adjacency List
 
 class Graph {
-    // Constructor: Initiate the Adjacency List here
-    Graph(AdjacencyListElement list) {
-
+    public static void createEdge(ArrayList<ArrayList<Integer>> list, int start, int end) {
+        list.get(start).add(end);
+        // list.get(end).add(start);
     }
 
-    // Constructor: Initiate Adjacency Matrix here - TODO
+    static void displayGraph(ArrayList<ArrayList<Integer>> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("\n (" + i + ")");
+            for (int j = 0; j < list.get(i).size(); j++) {
+                System.out.print(" - " + list.get(i).get(j));
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello Graphs!");
+        // Create the graph
+        int numOfVertices = 5;
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>(numOfVertices);
+        for (int i = 0; i < numOfVertices; i++) list.add(new ArrayList<Integer>());
+
+        // Add the edges
+        createEdge(list, 0, 1);
+        createEdge(list, 0, 2);
+        createEdge(list, 0, 3);
+        createEdge(list, 1, 2);
+        createEdge(list, 3, 2);
+
+        displayGraph(list);
     }
 }
